2e10eea684b108c2d25714082e071a5db00625c2
ztioge <egoitzu@telefonica.net>
2016-10-21 11:55:54 +0000

    a

--- app.js
+++ app.js
@@ -3,14 +3,6 @@ $(document).ready(function(){
 $('.navbar-nav li').click(function(item){$(this).siblings().removeClass('active');$(this).addClass('active');});
 
 //parallax
-$('.parallax-window').parallax();
-
-var options = [
-    {selector: '.nosotros', offset: 200, callback: customCallbackFunc },
-    {selector: '.proyectos', offset: 200, callback: function() {
-      customCallbackFunc();
-    } },
-  ];
-  Materialize.scrollFire(options);
+$('.parallax').parallax();
 });
 
